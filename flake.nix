e42ed9ac2969c5129612702781e1f24a83663284
mrkirby153 <mr.austinwhyte@gmail.com>
2023-05-10 17:18:36 -0700

    Add pseudo _all package for building all packages

--- flake.nix
+++ flake.nix
@@ -14,11 +14,29 @@
     flake-compat,
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    packages.x86_64-linux = import ./pkgs {
+    myPkgs = import ./pkgs {
       inherit pkgs;
       lib = pkgs.lib;
     };
+    getLeaves = (
+      attrset: let
+        values = pkgs.lib.attrValues attrset;
+        isLeaf = (v: (pkgs.lib.attrsets.isDerivation v));
+        leaves = pkgs.lib.filter isLeaf values;
+        nonLeaves = pkgs.lib.filter (v: (pkgs.lib.isAttrs v) && ! (pkgs.lib.attrsets.isDerivation v)) values;
+        nonLeavesLeaves = builtins.map (attr: getLeaves attr) nonLeaves;
+      in
+        pkgs.lib.concatLists [leaves (pkgs.lib.concatLists nonLeavesLeaves)]
+    );
+  in {
+    packages.x86_64-linux =
+      myPkgs
+      // {
+        _all = pkgs.symlinkJoin {
+          name = "all";
+          paths = (getLeaves myPkgs);
+        };
+      };
 
     formatter.x86_64-linux = pkgs.alejandra;
   };
