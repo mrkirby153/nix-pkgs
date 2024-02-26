caff7d544e29c09052cac24155074f350f895fc9
mrkirby153 <mr.austinwhyte@gmail.com>
2024-02-25 23:37:41 -0800

    Fix flake-compat

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
   outputs = {
     self,
     nixpkgs,
     rust-overlay,
+    flake-compat,
   } @ inputs: let
     pkgs = 
       import nixpkgs {
