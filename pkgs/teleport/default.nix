11020f302836460bae086f94917cb0f8a65e436e
mrkirby153 <mr.austinwhyte@gmail.com>
2024-03-28 17:11:39 -0700

    [teleport] teleport 15.1.10

--- pkgs/teleport/default.nix
+++ pkgs/teleport/default.nix
@@ -1,17 +1,17 @@
 { callPackage, inputs, ... }@args:
 callPackage ./generic.nix (rec {
-  version = "15.0.2";
-  hash = "sha256-xeDTaM2BuoSKff3WpBdkS5kBife5IGqLSK2wQQ1YqMo=";
-  vendorHash = "sha256-L7ZHMR/1O0vRO11qKkpeRcosy0c9skE8+pWvT3vDb7M=";
-  yarnHash = "sha256-mw00mLI0+wfz+easaY32RBgGt6DibImqT55VFrHK6dk=";
+  version = "15.1.10";
+  hash = "sha256-XAjfHAHgwGV3QX9BnnPe2NamXrnjWFNK1sUMR+UdEao=";
+  vendorHash = "sha256-y/umi3H5YZwHE8mnsz2Dq/+RxCMYOBB2InH3Di3sHOI=";
+  yarnHash = "sha256-4XV2wlXbSiPSG0gGw1DguIEBxwpFyKHEyyC4s3ggsjs=";
   cargoLock = {
     lockFile = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/gravitational/teleport/v${version}/Cargo.lock";
-      sha256 = "sha256:0f6qc6pnd1m9rfqwfxd6x2yxz43g60578vx18c84fgixc5b497hx";
+      sha256 = "sha256:0x4x50gxx9wr9m35jb8jrkn2sglvh34qqa85j725sqxvliwxg4w1";
     };
     outputHashes = {
       "boring-4.4.0" = "sha256-4wdl2kIA5oHQ0H6IddKQ+B5kRwrTeMbKe1+tAYZt2uw=";
-      "ironrdp-async-0.1.0" = "sha256-4XWrQtpZXNkGk9P+F1HcMYl6ZbAt/GPlpkIbfAH0xho=";
+      "ironrdp-async-0.1.0" = "sha256-0p0A3OBsWryUyhsefqrrkUvPOzSMQ2q4DirE4bZNvIw=";
       "sspi-0.10.1" = "sha256-fkclC/plTh2d8zcmqthYmr5yXqbPTeFxI1VuaPX5vxk=";
     };
   };
