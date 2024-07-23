f337877066156dd7fbd9bf6eef7f225e848491eb
mrkirby153 <mr.austinwhyte@gmail.com>
2024-07-22 22:32:39 -0700

    [teleport] Update teleport to 16.1.0

--- pkgs/teleport/default.nix
+++ pkgs/teleport/default.nix
@@ -1,21 +1,23 @@
-{ callPackage, inputs, ... }@args:
+{ callPackage, inputs, pkgs, ... }@args:
 callPackage "${inputs.nixpkgs}/pkgs/servers/teleport/generic.nix" (rec {
-  version = "16.0.1";
-  hash = "sha256-XTQwWV2tZ3JH2FyrRUlVJjeVH3rpq1My1LU6KtAIdfQ=";
-  vendorHash = "sha256-fcnxy0pzrJqS3pYefuEQPO/4ziHUhHNXuBhuH0kaNjE=";
-  yarnHash = "sha256-h478D+Ida/Gl7leljTi4XHzwugxU05TvNuylTv5Fc3U=";
+  version = "16.1.0";
+  hash = "sha256-uAyP2y2nbj9rxvWiKI/4SEOpOCozc/SRF0aHJMgQdhA=";
+  vendorHash = "sha256-3N87rhzlpuLbLD4EIMDf6GABnAiLZJ+TSLapj90Z1zw=";
+  yarnHash = "sha256-Q9b1Lmbl2elr7NMLjrTS21zssdZfeh5S1unaxjmGAxU=";
   cargoLock = {
     lockFile = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/gravitational/teleport/v${version}/Cargo.lock";
-      sha256 = "sha256:0777zldsx813b3yhckhpfgsdsz5syy8cxb162ps8d4w285zylfh9";
+      sha256 = "sha256:175cvg7jpfz22a9ig0psjkgxxww8cm68cs76v2slga3x53l6g3lm";
     };
     outputHashes = {
-      "boring-4.4.0" = "sha256-4wdl2kIA5oHQ0H6IddKQ+B5kRwrTeMbKe1+tAYZt2uw=";
-      "ironrdp-async-0.1.0" = "sha256-NpBzPsvopu5Te6Ljln5rp1Wxc6O6mRB4lLh0JVnN+Xc=";
-      "sspi-0.10.1" = "sha256-fkclC/plTh2d8zcmqthYmr5yXqbPTeFxI1VuaPX5vxk=";
+      "boring-4.7.0" = "sha256-ACzw4Bfo6OUrwvi3h21tvx5CpdQaWCEIDkslzjzy9o8=";
+      "ironrdp-async-0.1.0" = "sha256-nE5O/wRJ3vJqJG5zdYmpVkhx6JC6Yb92pR4EKSWSdkA=";
+      "sspi-0.10.1" = "sha256-fkclC/plTh2d8zcmqthYmr5yXqbPTeFxI1VuaPX5vxk";
     };
   };
-  extPatches = [
-    "${inputs.nixpkgs}/pkgs/servers/teleport/tsh_14.patch"
-  ];
-} // builtins.removeAttrs args [ "callPackage" "inputs" ])
+  wasm-bindgen-cli = pkgs.wasm-bindgen-cli.override {
+    version = "0.2.92";
+    hash = "sha256-1VwY8vQy7soKEgbki4LD+v259751kKxSxmo/gqE6yV0=";
+    cargoHash = "sha256-aACJ+lYNEU8FFBs158G1/JG8sc6Rq080PeKCMnwdpH0=";
+  };
+} // builtins.removeAttrs args [ "callPackage" "inputs" "pkgs" ])
